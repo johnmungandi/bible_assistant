16b56ede58de62df9d703632dd72d408c4954b66
John Mungandi <johnxpres@gmail.com>
2024-12-26 07:39:40 +0530

    App bar add homescreen

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: const Text('Home'),
+      )
     );
   }
 }
